9c9cc141c07ead73d002aedc72029da5f7638f61
Juan Oliver <juan_oliver@live.se>
2023-07-12 13:37:27 +0200

    Updated Time API ex. 1-3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the time checker");
+
+        System.out.println("#######################################################");
+
+        LocalDate today = LocalDate.now();
+
+        System.out.println("actual date : " + today);
+
+        System.out.println("#######################################################");
+
+        String custom = today.format(DateTimeFormatter.ofPattern("eeee dd MMM"));
+
+        System.out.println("" + custom);
+
+        System.out.println("#######################################################");
+
+        System.out.printf("Days since previous monday\n") ;
+
+        LocalDate lastMonday = today.with(DayOfWeek.MONDAY).minusWeeks(1);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+        for (int i = 0; i < 7; i++){
+
+            System.out.println(lastMonday.plusDays(i).format(formatter));
+
+        }
+
+        System.out.println("#######################################################");
+
+
     }
+
+
 }
